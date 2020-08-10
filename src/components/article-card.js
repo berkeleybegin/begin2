de04db720ac3af8163a4753cff4489e090e60e21
somaniarushi <arushisomani@berkeley.edu>
2020-08-09 19:36:22 -0700

    linking

--- src/components/article-card.js
+++ src/components/article-card.js
@@ -9,14 +9,14 @@ import PropTypes from "prop-types"
 class ArticleCard extends React.Component {
     render() {
         return (
-            <>
+            <a href="#">
             {this.props.spotlight &&
                 <p>EDITOR'S SPOTLIGHT</p>
             }
             <h1>{this.props.title}</h1>
             <p>{this.props.author} | {this.props.date}</p>
             <p>{this.props.excerpt}</p>
-            </>
+            </a>
         );
     }
 }
