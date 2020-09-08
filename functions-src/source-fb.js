402adf523d4b16272c6c8e19b5fee5b1267d7d66
Arushi Somani <arushisomani@berkeley.edu>
2020-09-07 19:41:57 -0700

    debugging sources-fb

--- functions-src/source-fb.js
+++ functions-src/source-fb.js
@@ -18,9 +18,9 @@ FB.setAccessToken(process.env.FB_KEY)
  * @param {Object} context: Provided by Netlify if Identity is enabled, contains a `clientContext` object with `identity` and `user` properties.
  * @param {netlifyCallback} callback: Defined like callback in an AWS Lambda function, used to return either an error, or a response object.
  */
-const sourceFB = (_event, _context, callback) => {
+const sourceFB = (event, _context, callback) => {
   const token = event.headers.Authorization.replace(/Bearer/i, "").trim()
-  jwt.verify(token, process.env.JWT_SECRET, error => {
+  jwt.verify(token, process.env.JWT_SECRET, (error) => {
     if (!error) {
       gh.init()
         .then(() => {
@@ -33,12 +33,12 @@ const sourceFB = (_event, _context, callback) => {
                 { method: "get", relative_url: "/techvista18/events" },
               ],
             },
-            function(response) {
+            function (response) {
               const filesToPush = []
 
-              response.forEach(item => {
+              response.forEach((item) => {
                 const file = JSON.parse(item.body)
-                file.data.forEach(event => {
+                file.data.forEach((event) => {
                   const date = moment(event.start_time).format("YYYY-MM-DD")
                   const id = uuidv4()
 
@@ -76,7 +76,7 @@ const sourceFB = (_event, _context, callback) => {
             }
           )
         })
-        .catch(err => {
+        .catch((err) => {
           callback(err)
         })
     } else {
