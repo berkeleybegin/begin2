d4f0459068afca7daf0c39bf0226dce88306a0be
Arushi Somani <arushisomani@berkeley.edu>
2021-04-23 00:25:59 -0700

    adding favicon and title

--- src/components/head.js
+++ src/components/head.js
@@ -1,10 +1,11 @@
 import React from "react"
-import Helmet from "react-helmet"
+import {Helmet} from "react-helmet"
 
 export default function Head() {
   return (
     <Helmet>
       <link rel="stylesheet" href="https://use.typekit.net/qko1qsv.css" />
+      <title>Begin Gateway To Innovation</title>
     </Helmet>
   )
 }
